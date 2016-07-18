0ca4c14a73c42be1bbc60cdccb000db90b9a4b53
classicemi <wushuang_1227@163.com>
2016-07-18 10:56:09 +0800

    delete unused files

--- test/test.js
+++ test/test.js
@@ -1,2 +1,12 @@
 pageConsole.init();
-console.log([1,2,3]);
+
+console.log('string: test string');
+console.log(1234567890);
+
+var arr = [1, 2, 3];
+console.log(arr);
+
+var obj = {
+  foo: 'bar'
+};
+console.log(obj);
